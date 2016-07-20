3be905920bb21994d45621d72cef0e9025480e43
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-07-20 15:58:28 -0300

    Preparing terrain to relate age and score to percentile

--- src/Raven/Infra/Calculator.fs
+++ src/Raven/Infra/Calculator.fs
@@ -10,7 +10,6 @@ let rec getColumnLoop(intervals: string[], index: int, age: int): int =
         else let raw = intervals.ElementAt(index).Split(' ')
              let lower = int(raw.ElementAt 0)
              let upper = int(raw.ElementAt 1)
-             Console.WriteLine(sprintf "%d %d %d" lower upper age)
              if age >= lower && age <= upper
                 then index
                 else getColumnLoop(intervals, index+1, age)
@@ -18,9 +17,24 @@ let rec getColumnLoop(intervals: string[], index: int, age: int): int =
 let getColumn(intervals: string[], age: int): int =
     getColumnLoop(intervals, 1, age)
 
+/// Gets the percentile based on choosen column and on the score
+// This logic is wrong!!! Must consider the upper and lower bounds
+let rec getPercentileLoop(table: string[][], score: int, row: int, column: int): int = 
+    if row = table.Length
+        then 1
+        else let value = int(table.ElementAt(row).ElementAt(column))
+             if value = score
+                then int(table.ElementAt(row).ElementAt(0))
+                else getPercentileLoop(table, score, row+1, column)
+
+let getPercentile(table: string[][], score: int, column: int): int = 
+    getPercentileLoop(table, 1, score, column)
+
 /// Takes a table and the number of correct answers and turn them into
 /// the percentile the subject belongs to.
 let CalculateResult(table: string[][], score: int, age: int): int =
-    getColumn(table.ElementAt 0, age)
+    let column = getColumn(table.ElementAt 0, age)
+    getPercentile(table, score, column)
+
 
 
