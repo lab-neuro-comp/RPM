ee34b136e33f8a8f89b4a75283d00fedd801374a
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-07-28 13:07:24 -0300

    Adding window before the test actually begin

--- src/Raven/Raven/View/FormIntro.cs
+++ src/Raven/Raven/View/FormIntro.cs
@@ -12,11 +12,8 @@ namespace Raven.View
         {
             InitializeComponent();
             WindowState = FormWindowState.Maximized;
-            PopulateItems();
-        }
-
-        private void PopulateItems()
-        {
+         
+            // Adicionando opções de testes   
             Cook = new Preparador();
             Cook.CarregarTestes();
             foreach(string item in Cook.Testes)
@@ -25,24 +22,27 @@ namespace Raven.View
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int idade;
             string nome;
+            string teste;
+            int idade;
 
             try
             {
-                idade = int.Parse(textIdade.Text);
                 nome = textNome.Text;
+                idade = int.Parse(textIdade.Text);
+                teste = Cook.Caminhos[comboOps.SelectedIndex];
+                
+                if (nome.Length < 1)
+                    throw new Exception();
             }
             catch (Exception any)
             {
+                MessageBox.Show("Preencha todos os campos", "Aviso!");
                 return;
             }
-                
-            FormOps f2 = new FormOps(new Aplicador(nome,
-                                                   Cook.Caminhos[comboOps.SelectedIndex], 
-                                                   int.Parse(textIdade.Text)));
-            f2.Show();
-            f2.Test();
+
+            FormPre form = new FormPre(new Aplicador(nome, teste, idade));
+            form.Show();
             this.Hide();
         }
     }
---
+++ src/Raven/Raven/View/FormPre.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Raven.View
+{
+    partial class FormPre
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(284, 261);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 130);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Aperte o botão abaixo para começar...";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(100, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Começar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormPre
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "FormPre";
+            this.Text = "FormPre";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
