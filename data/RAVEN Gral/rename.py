7fcc9191d834d167e2b4f4d08ec3f47c825cb79f
Cris Silva Jr <ishiikurisu@yahoo.com>
2016-09-14 16:05:22 -0300

    Renaming files correctly

--- data/RAVEN Gral/rename.py
+++ data/RAVEN Gral/rename.py
@@ -3,12 +3,12 @@ import re
 import sys
 
 def rename(inlet, pattern):
-    outlet = pattern.sub('.', inlet)
-    if inlet != outlet:
-        os.system('move %s %s' % (inlet, outlet))
+    match = pattern.search(inlet).group(0)
+    index = int(match[1:-1])
+    outlet = inlet.replace(match, '.%d.' % (index))
+    os.system('move %s %s' % (inlet, outlet))
 
 if __name__ == '__main__':
-    
-    pattern = re.compile('-')
+    pattern = re.compile(r'_[0-9].')
     for it in os.listdir('.'):
         rename(it, pattern)
